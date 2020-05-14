cb05d538111c6e5164a38471407fdae1d1bafbb3
nikrosss <nikros2008@mail.ru>
2020-05-14 16:06:27 +0300

    add create first conf

--- buildconfigufs.groovy
+++ buildconfigufs.groovy
@@ -4,8 +4,9 @@ node {
 
    stage('Preparation') { // for display purposes
 
-    println "start"
-
+        println "start"
+        git 'https://github.com/nikrosss/pipline.git'
+        bat (groovy 'build_conf_fp_name.groovy')
    }
    stage('Build') {
 
